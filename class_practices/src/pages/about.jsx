2a0b04dcfe20d5fcccfa0a9383fc0adf2a868f19
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-07-14 17:49:27 +0600

    Add about,home page navbar and modify app.js

---
+++ class_practices/src/pages/about.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>এইটা আমাদের ওয়েবসাইট সম্পর্কে পেজ।</p>
+    </div>
+  );
+};
+
+export default About;
